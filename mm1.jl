fc1bc6bc37f8f1802828fbfcd16525bf59621963
Viktor <vilyin@vilyin.(none)>
2014-02-24 14:31:07 +0800

    add type model

--- mm1.jl
+++ mm1.jl
@@ -1,10 +1,23 @@
-##
-a=[1 2 3 4 5 6]
-b = [2,3;4,5]
-function mmm()
- print(1)
+# Main program for single-server queueing system.
+const qlimit = 100
+type model
+    aniq:: Float64
+    autil:: Float64
+    busy:: Int
+    idle:: Int
+    marrvt:: Float64
+    mservt:: Float64
+    nevnts:: Int
+    next:: Int
+    niq:: Int
+    numcus:: Int
+    server:: Int
+    tarrvl:: Array{Float64,1}
+    time:: Float64
+    tlevnt:: Float64
+    tne:: Array{Float64,1}
+    totcus:: Int
+    totdel:: Float64 
 end
-#
-function mmm1()
- print(1)
-end
+
+
